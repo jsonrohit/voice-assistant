903afeed50331aae32ca65a3c7aec07997fffac0
Rohit Panchal <rohitpanchal@Rohits-MacBook-Air.local>
2026-09-02 03:14:33 +0530

    project setup

---
+++ main.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+# Load your .env file credentials
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(model="gemini-3.6-flash", temperature=0.7)
+response = llm.invoke("Hello")
+print(response.content)
